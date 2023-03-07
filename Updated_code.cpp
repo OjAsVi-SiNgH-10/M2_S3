5973081dae45a7922bfa48d5a7c659ff8431f572
OjAsVi-SiNgH-10 <103986788+OjAsVi-SiNgH-10@users.noreply.github.com>
2023-03-07 16:39:02 +0530

    Update Updated_code.cpp

--- Updated_code.cpp
+++ Updated_code.cpp
@@ -1,32 +1,37 @@
 #include <iostream>
 #include <stdlib.h>
-#include <time.h>
 #include <omp.h>
+#include <time.h>
+#include <unistd.h>
+#include<pthread.h>
+#include<thread>
 
-#define NUM_THREADS 8
+using namespace std;
 #define MAX 1000000
+
 
 int data[MAX];
 long global_sum = 0;
 
 int main(int argc, char *argv[])
-{ 
-    int cores = omp_get_num_procs();
-    omp_set_num_threads(NUM_THREADS);
+{
+    int cores = std::thread::hardware_concurrency();
+    cout << "The number of cores on this machine = " << cores << endl;
 
-    std::cout << "The number of cores on this machine = " << cores << std::endl;
-
-    srand(time(NULL));
-    for (int i = 0; i < MAX; i++) {
+    // srand(time(NULL));
+    for (int i = 0; i < MAX; i++)
+    {
         data[i] = rand() % 20;
     }
 
-    #pragma omp parallel for reduction(+:global_sum)
-    for (int i = 0; i < MAX; i++) {
-        global_sum += data[i];
+    #pragma omp parallel num_threads(cores) reduction(+:global_sum)
+    {
+        #pragma omp for
+        for (int i = 0; i < MAX; i++)
+        {
+            global_sum += data[i];
+        }
     }
 
-    std::cout << "The final sum = " << global_sum << std::endl;
-
-    return 0;
+    cout << "The final sum = " << global_sum << endl;
 }
